a08254f4c2b1dc282d553b59a60f780b12aa94c2
Steven Khaw <swkhaw8226@yahoo.com>
2022-11-06 19:26:37 -0800

    update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  return (JSON.parse(localStorage.getItem('recipes')));
 }
 
 /**
@@ -35,10 +36,29 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const main = document.querySelector('main');
+
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let i = 0; i < recipes.length; i++) {
+    const newRecipe = document.createElement('recipe-card');
+
+    newRecipe.data = {
+                      imgSrc: `${recipes[i].imgSrc}`,
+                      imgAlt: `${recipes[i].imgAlt}`,
+                      titleLnk: `${recipes[i].titleLnk}`,
+                      titleTxt: `${recipes[i].titleTxt}`,
+                      organization: `${recipes[i].organization}`,
+                      rating: `${recipes[i].rating}`,
+                      numRatings: `${recipes[i].numRatings}`,
+                      lengthTime: `${recipes[i].lengthTime}`,
+                      ingredients: `${recipes[i].ingredients}`
+                      };
+
+    main.append(newRecipe);
+  }
 }
 
 /**
@@ -51,6 +71,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+  
 }
 
 /**
